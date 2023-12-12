d6ef49a097288ef08065bece1ee3fd2fa21dc844
chris_wendi <72356458+christianwondeson@users.noreply.github.com>
2023-12-12 04:06:42 -0800

    Update newBot.ino
    telegram bot that monitors temperature and motion on data center.  with the help of esp8266 to connect to the network.

--- newBot/newBot.ino
+++ newBot/newBot.ino
@@ -1,50 +1,92 @@
-#include <DHT.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
 
-const char *ssid = "ba-wifi";
-const char *password = "1peter4:10";
-const char *token = "6817021114:AAFDrDSYzCT9OMjlbDg7iIk0c2R6UH7KFcU";
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-const int DHT_PIN = 13; // DHT11 data pin
-const int PIR_PIN = 14; // PIR motion sensor pin
+#define BOTtoken "6817021114:AAFDrDSYzCT9OMjlbDg7iIk0c2R6UH7KFcU"  // your Bot Token (Get from Botfather)
+#define CHAT_ID "395826160"
 
-DHT dht(DHT_PIN, DHT11);
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+const int motionSensor = 14;  // PIR Motion Sensor
+const int dhtPin = 12;        // DHT11 Data pin
+bool motionDetected = false;
+unsigned long lastUpdateTime = 0;
+const unsigned long updateInterval = 60000;  // 1 minute in milliseconds
+
+DHT dht(dhtPin, DHT11);
+
+void ICACHE_RAM_ATTR detectsMovement() {
+  motionDetected = true;
+}
+
+void sendStatus() {
+  float temperature = dht.readTemperature();
+  String statusMessage = "Motion: " + String(motionDetected ? "Detected" : "Not Detected") +
+                         "\nTemperature: " + String(temperature) + "°C";
+  bot.sendMessage(CHAT_ID, statusMessage, "");
+}
+
+void handleMessages() {
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  while (numNewMessages) {
+    for (int i = 0; i < numNewMessages; i++) {
+      String chat_id = String(bot.messages[i].chat_id);
+      String text = bot.messages[i].text;
+
+      if (text == "/start") {
+        bot.sendMessage(chat_id, "Hello! I am your motion and temperature bot. Use /status to get updates.");
+      } else if (text == "/status") {
+        sendStatus();
+      }
+
+      numNewMessages--;
+    }
+  }
+}
 
 void setup() {
-    Serial.begin(115200);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(250);
-        Serial.print(".");
-    }
-    Serial.println("WiFi connected");
+  Serial.begin(115200);
+  configTime(0, 0, "pool.ntp.org");
+  client.setTrustAnchors(&cert);
+
+  pinMode(motionSensor, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  dht.begin();
+
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
 }
 
 void loop() {
-    float temperature = dht.readTemperature();
-    int motion = digitalRead(PIR_PIN);
+  handleMessages();
+  unsigned long currentMillis = millis();
 
-    if (!isnan(temperature) && motion == HIGH) {
-        sendTelegramMessage(temperature);
-        delay(60000); // Send only one message per minute to avoid flooding
-    }
+  if (currentMillis - lastUpdateTime >= updateInterval) {
+    sendStatus();
+    lastUpdateTime = currentMillis;
+  }
 
-    delay(1000);
-}
-
-void sendTelegramMessage(float temperature) {
-    WiFiClientSecure client;
-    if (client.connect("api.telegram.org", 443)) {
-        String url = "/bot" + String(token) + "/sendMessage?chat_id=@YourChannelID&text=Temperature: " + String(temperature) + "C";
-        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                     "Host: api.telegram.org\r\n" +
-                     "Connection: close\r\n\r\n");
-        while (client.connected()) {
-            if (client.available()) {
-                String line = client.readStringUntil('\r');
-                Serial.print(line);
-            }
-        }
-        client.stop();
-    }
+  if (motionDetected) {
+    motionDetected = false;  // Reset the flag
+  }
 }
