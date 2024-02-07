0251afd20992ee561d1f3f3c1c1d49382b63a577
christianwondeson <christianwondeson22@gmail.com>
2024-02-07 12:58:03 +0300

    updated with database model and ORM

--- ch1-setup/blog/views.py
+++ ch1-setup/blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
-
-
-# dummy data for our blog post template
-posts=[
-    {
-        "author":"CoreyMs",
-        "title":"Blog Post 1",
-        "content":"First post content",
-        'date_posted':"august 27, 2018",
-    },
-    {
-        "author":"jane Doe",
-        "title":"Blog Post 2",
-        "content":"second post content",
-        'date_posted':"august 28, 2018",
-    },
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
-    context={
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+    context = {"posts": Post.objects.all()}
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return render(request, 'blog/about.html', {"title": "About"})
+    return render(request, "blog/about.html", {"title": "About"})
