52d0c7e1eba40f2bd81d9702e0ff60fac2b30658
christianwondeson <christianwondeson22@gmail.com>
2024-03-04 20:01:25 +0300

    updated with vercel

--- readingFiles.py
+++ readingFiles.py
@@ -1,32 +1,9 @@
-# with open('./python_coursera/words.txt', 'r') as f:
-#     for line in f.readlines():
-#         line.find("O")
-#         print(line.rstrip().upper())
-
-input = "()"
-
-
-def isValid(input):
-    stack = []
-    mapping = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-    }
-    for index, char in enumerate(input):
-        if char in mapping:
-            stack.append(char)
-            print(stack)
-            print(index, char)
-        elif stack and char == mapping[stack[-1]]:
-            print(index, char == mapping[stack[-1]])
-            stack.pop()
-            print(stack)
-        else:
-            return False
-    return not stack
-
-
-isValid(input)
-
+import re
+sum = 0
+with open('./python_coursera/words.txt', 'r') as f:
+    for line in f:
+        total = re.findall('\b[wW]\w+', line.rstrip())
+        for num in total:
+            sum += int(num)
+print(sum)
 # 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
