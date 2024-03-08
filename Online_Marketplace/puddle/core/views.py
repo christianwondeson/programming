eef5d745acfed974ab019b0ad401ae6fff4be1a2
christianwondeson <christianwondeson22@gmail.com>
2024-03-08 19:01:49 +0300

    updated with add, delete and edit item with further modifications

--- Online_Marketplace/puddle/core/views.py
+++ Online_Marketplace/puddle/core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 
+from .forms import signupForm
 # Create your views here. this are function based views that hand the content of the webpage based on the url route
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -13,3 +14,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = signupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login/')
+    else:
+        form = signupForm()
+    
+    return render(request, 'core/signup.html',{
+        'form': form,  
+    })
--- Online_Marketplace/puddle/puddle/urls.py
+++ Online_Marketplace/puddle/puddle/urls.py
@@ -26,7 +26,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index"),
-    path("contact/", contact, name="contact"),
-    path('items/', include('item.urls')),
+    path("", include("core.urls")),
+    path("dashboard/", include('dashboard.urls')),
+    path("items/", include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
