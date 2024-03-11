81b0614d402a3450ac15eac4fd5e8597016bd841
christianwondeson <christianwondeson22@gmail.com>
2024-03-11 12:52:34 +0300

    updated with edit from, browser filters, clear filters

--- Online_Marketplace/puddle/item/views.py
+++ Online_Marketplace/puddle/item/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
+from .models import Item, Category
+
+
+def items(request):
+    category_id = request.GET.get("category", 0)
+    query = request.GET.get("query", "")
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    return render(
+        request,
+        "item/items.html",
+        {
+            "items": items,
+            "query": query,
+            "categories": categories,
+            "category_id": int(category_id),
+        },
+    )
 
 
 # Create your views here.
@@ -29,7 +53,26 @@ def new(request):
         else:
             form = NewItemForm()
 
-    return render(request, "item/form.html", {"form": form})
+    return render(request, "item/form.html", {"form": form, "title": "New item"})
+
+
+@login_required
+def edit(request, pk):
+
+    form_class = EditItemForm
+    form = form_class(request.POST or None)
+
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    form = EditItemForm(request.POST, request.FILES, instance=item)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+
+            return redirect("item:detail", pk=item.id)
+        else:
+            form = EditItemForm(instance=item)
+
+    return render(request, "item/form.html", {"form": form, "title": "Edit Item"})
 
 
 @login_required
