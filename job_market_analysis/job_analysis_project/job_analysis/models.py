71e04acd93907c75f55416f82b5aaafe14b4dc4d
christianwondeson <christianwondeson22@gmail.com>
2024-04-01 22:56:47 +0300

    job analysis updated

--- job_market_analysis/job_analysis_project/job_analysis/models.py
+++ job_market_analysis/job_analysis_project/job_analysis/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
-class 
+class jobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    skills = models.TextField()
+    salary = models.CharField(max_length=50)
+    posting_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- job_market_analysis/job_analysis_project/job_analysis/views.py
+++ job_market_analysis/job_analysis_project/job_analysis/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import jobPosting
 
 # Create your views here.
+
+
+def job_list(request):
+    job_postings = jobPosting.objects.all()
+    return render(
+        request, "job_analysis/job_analysis.html", {"job_postings": job_postings}
+    )
